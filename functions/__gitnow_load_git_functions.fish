9104a04ddb300c08ffb2fa1fa7c1ca53df3f1115
Keksi <git@keksi.dev>
2026-07-20 00:19:47 +0200

    fix: fish now embeds completions in the binary

--- functions/__gitnow_load_git_functions.fish
+++ functions/__gitnow_load_git_functions.fish
@@ -3,6 +3,6 @@
 
 function __gitnow_load_git_functions -d "Gitnow: Load fish git functions on demand"
     if begin not type -q __fish_git_branches; or not type -q __fish_git_tags; end
-        source $__fish_data_dir/completions/git.fish
+        status get-file completions/git.fish | source
     end
 end
